928f9e74566d87b2781391732f8b43dda0266846
Giorgi.Gagnidze <giorgi.gagnidze@voidaint.com>
2024-07-03 20:19:00 +0400

    init data

---
+++ app/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.item (archived, discount, price, created_at, id, stock, updated_at, description, title)
+VALUES (false, 0.00, 14.60, '2024-07-03 20:01:28.000000', 1, 999, '2024-07-03 20:01:31.000000',
+        'Now available in convenient three-set packs, Ernie Ball Regular Slinky electric guitar strings are our top selling set.',
+        'Ernie Ball Regular Slinky Nickel Wound Electric Guitar Strings 3 Pack - 10-46 Gauge');
+
---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE public.item
+(
+    id          serial                   primary key,
+    title       text                        NOT NULL,
+    description text                        NOT NULL,
+    price       DECIMAL                     NOT NULL,
+    discount    DECIMAL,
+    stock       BIGINT                      NOT NULL,
+    archived    BOOLEAN DEFAULT FALSE,
+    created_at  TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    updated_at  TIMESTAMP WITHOUT TIME ZONE NOT NULL
+);
