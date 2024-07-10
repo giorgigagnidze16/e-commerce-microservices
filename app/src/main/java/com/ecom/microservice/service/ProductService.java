027b15991d5537beb9a48b078d7fd284992d9da6
Giorgi.Gagnidze <giorgi.gagnidze@voidaint.com>
2024-07-10 18:18:59 +0400

    get rid of interface

--- app/src/main/java/com/ecom/microservice/service/ProductService.java
+++ app/src/main/java/com/ecom/microservice/service/ProductService.java
@@ -3,18 +3,35 @@ package com.ecom.microservice.service;
 import java.util.List;
 
 import com.ecom.microservice.api.model.ProductResponse;
+import com.ecom.microservice.entity.Product;
+import com.ecom.microservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-/**
- * Product service.
- */
-public interface ProductService {
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository repository;
 
     /**
      * Finds all products in the database.
      *
      * @return product records from the database
-     *
      * @see ProductResponse
      */
-    List<ProductResponse> searchAll();
+    public List<ProductResponse> searchAll() {
+        return repository.findAll().stream().map(ProductService::mapToResponse).toList();
+    }
+
+    private static ProductResponse mapToResponse(Product product) {
+        return ProductResponse.builder()
+            .id(product.getId())
+            .stock(product.getStock())
+            .archived(product.getArchived())
+            .title(product.getTitle())
+            .description(product.getDescription())
+            .createdAt(product.getCreatedAt())
+            .updatedAt(product.getUpdatedAt())
+            .build();
+    }
 }
