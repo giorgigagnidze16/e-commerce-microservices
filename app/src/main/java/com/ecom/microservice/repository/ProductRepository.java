657279888f4d30c5c31223304451c90147ca8bd0
Giorgi.Gagnidze <giorgi.gagnidze@voidaint.com>
2024-07-15 20:45:20 +0400

    make product archivable

--- app/src/main/java/com/ecom/microservice/repository/ProductRepository.java
+++ app/src/main/java/com/ecom/microservice/repository/ProductRepository.java
@@ -1,7 +1,33 @@
 package com.ecom.microservice.repository;
 
-import com.ecom.microservice.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import com.ecom.microservice.entity.Product;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ * Product repository
+ */
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    /**
+     * Finds all active products in the market.
+     *
+     * @return List of found products
+     */
+    List<Product> findAllByArchived(boolean archived, PageRequest pageRequest);
+
+    /**
+     *
+     * @param id of product
+     * @param archived new product status true/false
+     * @return number of updated rows
+     * @see Product
+     */
+    @Modifying
+    @Query("UPDATE Product p set p.archived= :archived where p.id = :id")
+    int updateVisibility(Long id, boolean archived);
+
 }
