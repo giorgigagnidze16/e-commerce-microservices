ccb2ed23bc02d85cab080fc447df0637e54a9272
Giorgi.Gagnidze <giorgi.gagnidze@voidaint.com>
2024-07-15 20:44:44 +0400

    add exception handler

---
+++ api/src/main/java/com/ecom/microservice/api/exception/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.ecom.microservice.api.exception;
+
+/**
+ * Exception thrown whenever resource was not found.
+ */
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(Throwable t, String message) {
+        super(message, t);
+    }
+}
