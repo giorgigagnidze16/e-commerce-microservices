20106713720d19c74877ef3fbba30eadf442ced4
Giorgi <giorgigagnidze16@gmail.com>
2024-08-01 01:56:33 +0400

    Set stateless session policy

--- app/src/main/java/com/ecom/microservice/web/config/WebSecurityConfig.java
+++ app/src/main/java/com/ecom/microservice/web/config/WebSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,12 +15,16 @@ public class WebSecurityConfig {
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http
-            .authorizeHttpRequests(c -> c
-                .requestMatchers(HttpMethod.GET, "/product").permitAll()
-                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                .anyRequest()
-                .authenticated()
-            )
-            .build();
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(configurer ->
+                        configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(c -> c
+                        .requestMatchers(HttpMethod.GET, "/product").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .build();
     }
 }
